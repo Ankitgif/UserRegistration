ff51bb104430d62e7858dee676706f68b3978d36
Ankitgif <ankitsinha2303@gmail.com>
2020-04-25 00:22:25 +0530

    [Ankit] Added Validation for Password Rule 1

--- src/main/java/com/sample/UserValidator.java
+++ src/main/java/com/sample/UserValidator.java
@@ -7,6 +7,7 @@ public class UserValidator {
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
     private static final String EMAIL_PATTERN = "^[a-z]{3,}([._+-][a-z0-9]*)?[@]{1}[a-z]{2,}[.]{1}[a-z]{2,4}(.[a-z]{2})?$";
     private static final String MOBILE_PATTERN = "^[0-9]{2}[ :space: ][0-9]{10}";
+    private static final String PASSWORD_PATTERN = "^[a-zA-z]{8,}$";
 
     public boolean validateFirstName(String fname) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -23,5 +24,9 @@ public class UserValidator {
     public boolean validateMobileNumber(String number) {
         Pattern pattern = Pattern.compile(MOBILE_PATTERN);
         return pattern.matcher(number).matches();
+    }
+    public boolean validatePassword(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        return pattern.matcher(password).matches();
     }
 }
