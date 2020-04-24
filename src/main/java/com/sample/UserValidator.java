82c44417dcdba9768599feaa4770e839e22b8537
Ankitgif <ankitsinha2303@gmail.com>
2020-04-24 23:59:05 +0530

    [Ankit] Added Validation for Mobile Format

--- src/main/java/com/sample/UserValidator.java
+++ src/main/java/com/sample/UserValidator.java
@@ -6,6 +6,7 @@ public class UserValidator {
 
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
     private static final String EMAIL_PATTERN = "^[a-z]{3,}([._+-][a-z0-9]*)?[@]{1}[a-z]{2,}[.]{1}[a-z]{2,4}(.[a-z]{2})?$";
+    private static final String MOBILE_PATTERN = "^[0-9]{2}[ :space: ][0-9]{10}";
 
     public boolean validateFirstName(String fname) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -18,5 +19,9 @@ public class UserValidator {
     public boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matcher(email).matches();
+    }
+    public boolean validateMobileNumber(String number) {
+        Pattern pattern = Pattern.compile(MOBILE_PATTERN);
+        return pattern.matcher(number).matches();
     }
 }
